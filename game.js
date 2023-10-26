3885cc1937e49321fcb53b390a22160f551021d9
zhmou <948508913@qq.com>
2023-10-26 17:08:24 +0800

    10/20 basic update

--- game.js
+++ game.js
@@ -45,16 +45,16 @@ class Blocks {
 }
 
 class RainbowBlockBreaker {
+    static WIDTH = 465;
+    static HEIGHT = 465;
     constructor() {
-        const WIDTH = 465;
-        const HEIGHT = 100;
         this.canvas = document.getElementById("gameCanvas");
         this.ctx = this.canvas.getContext("2d");
-        this.blocks = new Blocks(WIDTH, HEIGHT);
-        this.fallBlocks = new Array(0);
+        this.blocks = new Blocks(RainbowBlockBreaker.WIDTH, 100);
+        this.fallBlocks = new Array();
         this.bar = { width: 50, height: 10 };
-        this.bar.x = (WIDTH - this.bar.width) / 2;
-        this.bar.y = WIDTH - this.bar.width;
+        this.bar.x = (RainbowBlockBreaker.WIDTH - this.bar.width) / 2;
+        this.bar.y = RainbowBlockBreaker.WIDTH - this.bar.width;
         this.ctx.fillStyle = "#00DD00";
         this.ctx.fillRect(this.bar.x, this.bar.y, this.bar.width, this.bar.height);
         const ball = new Particle(465 / 2, 465 / 2);
@@ -63,27 +63,115 @@ class RainbowBlockBreaker {
         ball.color = "#FFFFFF"
         this.balls = new Array();
         this.balls.push(ball);
+        document.addEventListener("mousemove", (e) => {
+            this.ctx.fillStyle = "#000000";
+            this.ctx.fillRect(this.bar.x, this.bar.y, this.bar.width, this.bar.height);
+
+            this.bar.x = e.clientX - this.canvas.offsetLeft - this.bar.width / 2;
+            if (this.bar.x > RainbowBlockBreaker.WIDTH - this.bar.width) {
+                this.bar.x = RainbowBlockBreaker.WIDTH - this.bar.width;
+            } else if (this.bar.x < 0) {
+                this.bar.x = 0;
+            }
+            this.ctx.fillStyle = "#00DD00";
+            this.ctx.fillRect(this.bar.x, this.bar.y, this.bar.width, this.bar.height);
+        })
     }
 
     update() {
-        this.blocks.values.forEach(block => {
+        const _this = this;
+        _this.blocks.values.forEach(block => {
             if (block) {
-                this.ctx.putImageData(new ImageData(new Uint8ClampedArray([block.color[0], block.color[1], block.color[2], 255]), 1, 1), block.x, block.y);
+                _this.ctx.putImageData(new ImageData(new Uint8ClampedArray([block.color[0], block.color[1], block.color[2], 255]), 1, 1), block.x, block.y);
             }
         });
-        const removeBalls = new Array();
-        this.balls.forEach(ball => {
-            ballSpeed = Math.sqrt(ball.vx * ball.vx + ball.vy * ball.vy);
-            ballRadius = Math.atan2(ball.vy, ball.vx);
+        const removedBalls = new Array();
+        _this.balls.forEach(function (ball) {
+            const ballSpeed = Math.sqrt(ball.vx * ball.vx + ball.vy * ball.vy);
+            const ballRadius = Math.atan2(ball.vy, ball.vx);
+            for (let i = 0; i < ballSpeed; i++) {
+                ball.x += ball.vx / ballSpeed;
+                ball.y += ball.vy / ballSpeed;
+                const hitParticle = _this.blocks.getParticle(ball.x, ball.y);
+                if (hitParticle) {
+                    const removedParticle = _this.blocks.removeParticle(ball.x, ball.y);
+                    removedParticle.vx = Math.cos(ballRadius + Math.PI * 2 / (30 * Math.random()) - 15) * 3;
+                    removedParticle.vy = 1;
+                    removedParticle.color = hitParticle.color;
+                    _this.fallBlocks.push(removedParticle);
+                    ball.vy = -ball.vy;
+                }
+                if ((ball.x < 0 && ball.vx < 0) || (ball.x > RainbowBlockBreaker.WIDTH && ball.vx > 0)) {
+                    ball.vx = -ball.vx;
+                }
+                if (ball.y < 0 && ball.vy < 0) {
+                    ball.vy = -ball.vy;
+                }
+                if (ball.y > RainbowBlockBreaker.HEIGHT) {
+                    removedBalls.push(ball);
+                }
+                if (RainbowBlockBreaker.hitTestPoint(_this.bar, ball)) {
+                    ball.vy = -Math.abs(ball.vy);
+                }
+                _this.ctx.putImageData(new ImageData(new Uint8ClampedArray([ball.color[0], ball.color[1], ball.color[2], 255]), 1, 1), ball.x, ball.y);
+            }
         })
+        removedBalls.forEach(function (particle) {
+            const index = _this.balls.indexOf(particle)
+            if (index !== -1) {
+                _this.balls.splice(index, 1);
+            }
+        })
+
+        const removedFallBlocks = new Array();
+        _this.fallBlocks.forEach(function (fallParticle) {
+            fallParticle.vy += 0.1;
+            fallParticle.x += fallParticle.vx;
+            fallParticle.y += fallParticle.vy;
+            _this.ctx.putImageData(new ImageData(new Uint8ClampedArray([fallParticle.color[0], fallParticle.color[1], fallParticle.color[2], 255]), 1, 1), fallParticle.x, fallParticle.y);
+            if (RainbowBlockBreaker.hitTestPoint(_this.bar, fallParticle)) {
+                const newBall = new Particle(fallParticle.x, fallParticle.y);
+                newBall.vx = Math.random() * 10;
+                newBall.vy = Math.random() * 9 + 1;
+                newball.color = fallParticle.color;
+                _this.balls.push(newball);
+                removedFallBlocks.push(fallParticle);
+            } else if (fallParticle.y > RainbowBlockBreaker.HEIGHT) {
+                removedFallBlocks.push(fallParticle);
+            }
+        })
+
+        removedFallBlocks.forEach(function (block) {
+            const index = _this.fallBlocks.indexOf(block);
+            if (index !== -1) {
+                _this.fallBlocks.splice(index, 1);
+            }
+        })
+
+        if (_this.blocks.count === 0) {
+            alert("Game End");
+            return
+        }
+
+        requestAnimationFrame(() => _this.update());
+    }
+
+    static hitTestPoint(Rect, Point) {
+        if (Point.x >= Rect.x && Point.x <= Rect.x + Rect.width && Point.y >= Rect.y && Point.y <= Rect.y + Rect.height) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
 
 const game = new RainbowBlockBreaker()
+document.addEventListener("DOMContentLoaded", () => { game.update() });
+
 
 function hsv2rgb(h, s, v) {
     let f = (n, k = (n + h / 60) % 6) => v - v * s * Math.max(Math.min(k, 4 - k, 1), 0);
-    return [f(5), f(3), f(1)];
+    return [f(5) * 255, f(3) * 255, f(1) * 255];
 }
 
 
